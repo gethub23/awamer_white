958088bf0f83cefc926522e05afd647f100c586b
gethub23 <aa926626@gmail.com>
2021-10-24 10:25:55 +0200

    home menu

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -3,22 +3,22 @@
     <section class="content">
         <div class="row">
             @foreach($menus as $menu)
-            <div class="col-lg-2">
-                <div class="price_card">
-                    <div class="row align-items-center justify-content-center">
-                        <div class="col-12 text-center">
-                            
-                            {!! $menu['icon'] !!}
-                        </div>
-                        <div class="col-12">
-                            <div class="title text-center">
-                                <h3>{{$menu['count']}}</h3>
-                                <span>{{$menu['name']}}</span>
+                <a href="{{$menu['url']}}" class="col-lg-2">
+                    <div class="price_card">
+                        <div class="row align-items-center justify-content-center">
+                            <div class="col-12 text-center">
+                                
+                                {!! $menu['icon'] !!}
+                            </div>
+                            <div class="col-12">
+                                <div class="title text-center">
+                                    <h3 style="color: #767676">{{$menu['count']}}</h3>
+                                    <span>{{$menu['name']}}</span>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                </a>
             @endforeach
         </div>
     </section>
