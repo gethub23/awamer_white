d687df4b9153d4470ae0a7522beba9d0e40f9041
gethub23 <aa926626@gmail.com>
2021-11-07 15:47:20 +0200

    notifications ثىي

--- app/Http/Controllers/Admin/NotificationController.php
+++ app/Http/Controllers/Admin/NotificationController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Jobs\Notify;
 use App\Models\User;
+use App\Jobs\SendSms;
 use App\Models\Admin;
 use App\Jobs\AdminNotify;
+use App\Jobs\SendEmailJob;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -32,11 +34,16 @@ class NotificationController extends Controller
         }else if($request->user_type == 'admins'){
             $rows = Admin::get() ; 
         }
-        
-        if ($request->user_type == 'admins') {
-            dispatch(new AdminNotify($rows, $request));
+        if ($request->type == 'notify') {
+            if ($request->user_type == 'admins') {
+                dispatch(new AdminNotify($rows, $request));
+            }else{
+                dispatch(new Notify($rows, $request));
+            }
+        }else if ($request->type == 'email') {
+            dispatch(new SendEmailJob($rows->pluck('email'), $request));
         }else{
-            dispatch(new Notify($rows, $request));
+            dispatch(new SendSms($rows->pluck('phone')->toArray() , $request->message));
         }
 
         return response()->json() ; 
